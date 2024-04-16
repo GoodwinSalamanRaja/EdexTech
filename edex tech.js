7b57c5896da745aabba60f930c15f2e56bd851b9
GoodwinSalamanRaja <20cos112@americancollege.edu.in>
2024-04-16 15:13:00 +0530

    my first project using html and css alone

---
+++ edex tech.js
@@ -0,0 +1,56 @@
+        // method 1
+// var range=document.getElementById("range");
+// function fundamentals(){
+//     document.getElementById("fundamentals");
+//     range.value=1 ;
+// }
+// function web(){
+//     document.getElementById("webdevelopment");
+//     range.value=2 ;
+// }
+// function vue(){
+//     document.getElementById("vuejs");
+//     range.value=3 ;
+// }
+// function react(){
+//     document.getElementById("reactjs");
+//     range.value=4 ; 
+// }
+// function node(){
+//     document.getElementById("nodejs");
+//     range.value=5 ;
+// }
+// function spring(){
+//     document.getElementById("springboot");
+//     range.value=6 ;
+// }
+// function building(){
+//     document.getElementById("buildingand");
+//     range.value=7 ;
+// }
+      // ,method 2  
+var range=document.getElementById("range")
+function rangefun(x){
+    console.log("range")
+    if(x==1){
+        range.value=1
+    } 
+    if(x==2){
+        range.value=2
+    }
+    if(x==3){
+        range.value=3
+    }
+    if(x==4){
+        range.value=4
+    }
+    if(x==5){
+        range.value=5
+    }
+    if(x==6){
+        range.value=6
+    }
+    if(x==7){
+        range.value=7
+    }
+}
